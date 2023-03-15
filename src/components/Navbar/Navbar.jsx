549f81791a8dcc11217358a3b7eedfbb13570a75
Rachel Lam <rachellam@Rachels-MacBook-Pro.local>
2023-03-15 12:48:45 +0900

    feat: navbar style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 
@@ -13,33 +13,47 @@ const navItemsEl = navItems.map((item, index) => (
   </li>
 ));
 
-const Navbar = () => (
-  <div className="app__navbar">
-    <div className="app__navbar-logo">
-      <img src={images.gericht} alt="Gericht Logo" />
-    </div>
-    <ul className="app__navbar-links">{navItemsEl}</ul>
-    <div className="app__navbar-login">
-      <a href="#login" className="p__opensans">
-        Login / Register
-      </a>
-      <div />
-      <a href="/" className="p__opensans">
-        Book table
-      </a>
-    </div>
-    <div className="app__navbar-smallscreen">
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
-      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-        <MdOutlineRestaurantMenu
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <div className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="Gericht Logo" />
+      </div>
+      <ul className="app__navbar-links">{navItemsEl}</ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Login / Register
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Book table
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
           fontSize={27}
-          className="overlay__close"
-          onClick={() => {}}
+          onClick={() => {
+            setShowMenu(true);
+          }}
         />
-        <ul className="app__navbar-smallscreen-links">{navItemsEl}</ul>
+        {showMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => {
+                setShowMenu(false);
+              }}
+            />
+            <ul className="app__navbar-smallscreen_links">{navItemsEl}</ul>
+          </div>
+        )}
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
