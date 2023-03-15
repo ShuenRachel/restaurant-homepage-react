889db7bf16c89a9119c4a0264629643218e334ad
Rachel Lam <rachellam@Rachels-MacBook-Pro.local>
2023-03-15 12:28:59 +0900

    feat: navbar html

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+
+import images from "../../constants/images";
+import "./Navbar.scss";
+
+const navItems = ["home", "about", "menu", "awards", "contact"];
+
+const navItemsEl = navItems.map((item, index) => (
+  <li key={item + index} className="p__opensans">
+    <a href={`#${item}`}>{item}</a>
+  </li>
+));
+
+const Navbar = () => (
+  <div className="app__navbar">
+    <div className="app__navbar-logo">
+      <img src={images.gericht} alt="Gericht Logo" />
+    </div>
+    <ul className="app__navbar-links">{navItemsEl}</ul>
+    <div className="app__navbar-login">
+      <a href="#login" className="p__opensans">
+        Login / Register
+      </a>
+      <div />
+      <a href="/" className="p__opensans">
+        Book table
+      </a>
+    </div>
+    <div className="app__navbar-smallscreen">
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
+      <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+        <MdOutlineRestaurantMenu
+          fontSize={27}
+          className="overlay__close"
+          onClick={() => {}}
+        />
+        <ul className="app__navbar-smallscreen-links">{navItemsEl}</ul>
+      </div>
+    </div>
+  </div>
+);
+
+export default Navbar;
